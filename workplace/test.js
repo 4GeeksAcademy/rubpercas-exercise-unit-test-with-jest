33f22f1df4b494452e6cd1fd1aaa924f3d1661b2
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2024-10-11 18:33:33 +0000

    Done

--- workplace/test.js
+++ workplace/test.js
@@ -9,14 +9,14 @@ test("1 euro should be 1.07 dollars", function() {
     const expected = 3.5 * 1.07;
 
     // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(fromEuroToDollar(3.5)).toBe(3.745);
 })
 test("1.07 dollars should be 156.5 yens", function() {
     const { fromDollartoYen } = require('./app.js');
     const yens = fromDollartoYen(1.07);
     const expected = 156.5;
 
-    expect(fromDollartoYen(1.07)).toBe(156.5); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(fromDollartoYen(1.07)).toBe(156.5); 
 })
 test("156.5 Yens should be 0.87 pounds", function() {
     const { fromYentoPound } = require('./app.js');
