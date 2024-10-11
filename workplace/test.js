7462e0198a287f26c7104f58c557c575d42f598e
RubenPerezCastrillo <ruben.perez.castrillo@gmail.com>
2024-10-11 19:12:51 +0000

    Added compare input conditionals to be sure the input is a number

--- workplace/test.js
+++ workplace/test.js
@@ -1,27 +1,27 @@
 test("1 euro should be 1.07 dollars", function() {
     // Importo la funcion desde app.js
     const { fromEuroToDollar } = require('./app.js');
+    let inputValue = "a";
+    if (typeof inputValue != "number") {
+        throw new Error("Invalid input: Please enter a valid number. (In this example, it has to be 1)");
+      }
 
-    // Uso la función como debe ser usada
-    const dollars = fromEuroToDollar(3.5);
+    const dollars = fromEuroToDollar(inputValue);
+    const expected = 1.07;
 
-    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = 3.5 * 1.07;
-
-    // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745);
+    expect(dollars).toBe(expected);
 })
 test("1.07 dollars should be 156.5 yens", function() {
     const { fromDollartoYen } = require('./app.js');
     const yens = fromDollartoYen(1.07);
     const expected = 156.5;
 
-    expect(fromDollartoYen(1.07)).toBe(156.5); 
+    expect(yens).toBe(expected); 
 })
 test("156.5 Yens should be 0.87 pounds", function() {
     const { fromYentoPound } = require('./app.js');
     const pounds = fromYentoPound(156.5);   
     const expected = 0.87;
 
-    expect(fromYentoPound(156.5)).toBe(0.87); 
+    expect(pounds).toBe(expected); 
 })
